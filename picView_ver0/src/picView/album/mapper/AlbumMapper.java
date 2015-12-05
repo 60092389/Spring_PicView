30e3886a0a4fbdd87cc4784af5a92a0d5ddc7b44
yeonsik <bbangsik4508@naver.com>
2015-12-05 13:43:13 +0900

    testtest

--- picView_ver0/src/picView/album/mapper/AlbumMapper.java
+++ picView_ver0/src/picView/album/mapper/AlbumMapper.java
@@ -1,6 +1,23 @@
 package picView.album.mapper;
 
+import java.util.List;
+
+import picView.album.model.Album;
+import picView.album.model.Group_Pic;
+import picView.picture.model.Picture;
 
 public interface AlbumMapper {
+	
+	public List<Album> albumlist(int mem_no);
+	public List<Group_Pic> grouplist(int alb_no);
+	public List<Picture> albumpiclist(int pic_no);
+	public List<Picture> piclist(int mem_no);
+	public void insertAlbum(Album album);
+	public Album selectAlbum_no(String alb_word);
+	public void insertGroup(Group_Pic group);
+	public Album detailAlbum(int alb_no);
+	public Picture selectPicture(int pic_no);
+	public void deleteGroup(int alb_no);
+	public void deleteAlbum(int alb_no);
 
 }
